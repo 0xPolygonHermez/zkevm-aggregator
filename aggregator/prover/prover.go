be80bf2d78c5ec8734e8564118dc408801974afc
Toni Ramírez <toni@polygon.technology>
2024-08-14 12:16:54 +0200

    refactor batch sanity check

--- aggregator/prover/prover.go
+++ aggregator/prover/prover.go
@@ -265,7 +265,11 @@ func (p *Prover) WaitRecursiveProof(ctx context.Context, proofID string) (string
 	}
 
 	sr, err := GetStateRootFromProof(res.Proof.(*GetProofResponse_RecursiveProof).RecursiveProof)
-	if err != nil {
+	if err != nil && sr != (common.Hash{}) {
+		log.Errorf("Error getting state root from proof: %v", err)
+	}
+
+	if sr == (common.Hash{}) {
 		log.Info("Recursive proof does not contain state root. Possibly mock prover is in use.")
 	}
 
