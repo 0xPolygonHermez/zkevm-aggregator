19e5fca086dddc1bc8501b6e3b684d12b50018d5
Toni Ramírez <toni@polygon.technology>
2024-08-22 09:47:19 +0200

    fix sanity check

--- aggregator/aggregator.go
+++ aggregator/aggregator.go
@@ -860,7 +860,7 @@ func (a *Aggregator) buildFinalProof(ctx context.Context, proverI proverInterfac
 			return nil, fmt.Errorf("failed to retrieve batch with number [%d]", proof.BatchNumberFinal)
 		}
 
-		if common.BytesToHash(finalProof.Public.NewStateRoot).String() != finalDBBatch.Batch.StateRoot.String() {
+		if common.BytesToHash(finalProof.Public.NewStateRoot) != finalDBBatch.Batch.StateRoot {
 			for {
 				log.Errorf("State root from the final proof does not match the expected for batch %d: Proof = [%s] Expected = [%s]", proof.BatchNumberFinal, common.BytesToHash(finalProof.Public.NewStateRoot).String(), finalDBBatch.Batch.StateRoot.String())
 				time.Sleep(a.cfg.RetryTime.Duration)
